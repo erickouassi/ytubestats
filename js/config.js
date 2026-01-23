e8c9fff69412599e81e8d27cea070edc2cc5d473
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2026-01-23 09:57:13 -0500

    Refactor config.js to obfuscate sensitive data

--- js/config.js
+++ js/config.js
@@ -16,9 +16,157 @@ Use the commented code above to replace the code in the section below.
 Make sure the replacement is exact and preserves all functionality.  
 */
 
-// config.js
-export const CHANNEL_ID = 'UCEC5OcghM849ZBAbrSr8EUw';  // Replace with your Channel ID
-export const API_KEY    = 'AIzaSyAKoKpvnzeLINfB5r1NSC2H-Bnv1SooA3I';  // Replace with your API Key
+var uUVzEW, pPxMws, wT_0CKB, Kwa81K2, YrC5JZ, xzj_k1, CtAVWqv, sVHdSN, ZeRobH;
+const nDFAuq = [0x0, 0x1, 0x8, 0xff, "length", "undefined", 0x3f, 0x6, "fromCodePoint", 0x7, 0xc, "push", void 0x0, !0x1, !0x0, 0x5b, 0x1fff, 0x58, 0xd, 0xe];
+
+function OzFXoMc(uUVzEW) {
+    var pPxMws = "iDoMGAtkQJzq@mU^ONy_*2R.s8/5Yx%C+Xwf4l}ISFLWV3cg$vd{eZ[uT|p<H7h9~aPr;B)K&bjn0>,:`16!(=\"#?]E",
+        wT_0CKB, Kwa81K2, YrC5JZ, xzj_k1, CtAVWqv, sVHdSN, ZeRobH;
+    j7RVEld(wT_0CKB = "" + (uUVzEW || ""), Kwa81K2 = wT_0CKB.length, YrC5JZ = [], xzj_k1 = nDFAuq[0x0], CtAVWqv = nDFAuq[0x0], sVHdSN = -nDFAuq[0x1]);
+    for (ZeRobH = nDFAuq[0x0]; ZeRobH < Kwa81K2; ZeRobH++) {
+        var OzFXoMc = pPxMws.indexOf(wT_0CKB[ZeRobH]);
+        if (OzFXoMc === -nDFAuq[0x1]) continue;
+        if (sVHdSN < nDFAuq[0x0]) {
+            sVHdSN = OzFXoMc
+        } else {
+            j7RVEld(sVHdSN += OzFXoMc * nDFAuq[0xf], xzj_k1 |= sVHdSN << CtAVWqv, CtAVWqv += (sVHdSN & nDFAuq[0x10]) > nDFAuq[0x11] ? nDFAuq[0x12] : nDFAuq[0x13]);
+            do {
+                j7RVEld(YrC5JZ.push(xzj_k1 & nDFAuq[0x3]), xzj_k1 >>= nDFAuq[0x2], CtAVWqv -= nDFAuq[0x2])
+            } while (CtAVWqv > nDFAuq[0x9]);
+            sVHdSN = -nDFAuq[0x1]
+        }
+    }
+    if (sVHdSN > -nDFAuq[0x1]) {
+        YrC5JZ.push((xzj_k1 | sVHdSN << CtAVWqv) & nDFAuq[0x3])
+    }
+    return RMs5_N(YrC5JZ)
+}
+
+function _XxNov7(wT_0CKB) {
+    if (typeof uUVzEW[wT_0CKB] === nDFAuq[0x5]) {
+        return uUVzEW[wT_0CKB] = OzFXoMc(pPxMws[wT_0CKB])
+    }
+    return uUVzEW[wT_0CKB]
+}
+j7RVEld(uUVzEW = {}, pPxMws = ["c;&b.}Jn3qKr,Bl8I.V>#<cpv9~2I/M40.SJw;M1;[l", "^@hC.|Z9B_D[VM[S8D", "fHO{>1$Sy9BHM):I*+sXoh&co|C<D", "8+R0)wc%O|(xzA", "$t8nTH&pN|*wxG&lhaKv*hhy,^ya[vDN_9o", "&$U0PhGytuV`*$8x!<tXK:?e+2$GO!/xF$90/", "U|)YT&9xD[;>}UiL0<#[njx|Tp+074lseKN%x<oy+2L", "YaNtn|_xQu9P8O;}2xmu`0iysazMol@", "()}GWflmHTxh&KL/v3(ZE[a91J,>mO/f&{D", "I@>KK4ep;JwM>&/xqD", "~N:ei4SpnU&>t}vS)MO[>Bmg+@", "V@&JZpd%4JnjS&iWBLHzV9%@6HVPL8llN[?t>0i", "EUex~cbxt", "aC3ko\"Yj^2(9Io\"fxS)eR}fxTmjj))8FV@Vxy", ">8Zz\"0cy{~st^oI8F$rZGwOc^Hd", "7<.J[>Zx)m/", "pw!+LTOe=~;b:Kg/x^Cvcl{gnNB)H5F8LKw{c`pc3Qf&)mrsR|}GmjNm!@|`i", "@+\"GMhOcHT@5x$(SBJL+bH=Ng9!9BH:I_HD", "mG(Z4n^1~@7", "8[UJZTpo", "G9L+sei", "aMmC+g0e:T92OMcIDGiJrcty^^c0|7z8|i", "*+IG/Sq_@h*ls<oyoWtuZT%4]p*w:U&},l~vup\";Q*hTk}GsiP@z", "T{EQu9Gs6_~D(K?LiJc0UdeS?*`Z^oP.$KTtW);s7<18F$N*<$lC`jKxk", "sKkt9(%sfHfBV<ZY;[mu\"3Ho", "4/%uh&>{u*|", "9J0d14ufA[=Rm&dwa.o", ">ahJk\"[SgQFf:/=Fu{Q{N|hc*m&Hv$uwiG3z${8Zvyw&sKO5X@g0\"3]D", "SMD57H[S&a:R:/o4d[GjbB*e$|n`LmB8J|[J%bE@HQZ", "ylpQ{luD", "|NiJFbZg5</5=s^5B;0K$(!mf@=TW}|YSa@js<u.bNdT./R59to", "C[gt[ID#]a=>b&G}h<{&sgL~i@3e{Uc_C.d&&19fk*1QMdyS1wP+[),\"]N", "F;wnn$+~,yC:0^i%i9!Q{{1{D^@6[vaN;t,G&0gn+P|", "t@d+p&z)Am.6K}F8JG[eY!511Jjr/dPl*9RuV`1mzu8", "F/w+0};p[_lw\"B~yOlBKLfJk8apviIPlif!AJH7xkp;`;MXsVKZCseBD", "?ww+^B3g0N", "CJEQ$p3k1JPZ6BiL[JIJ):E\"a|t.UA", "Shf&Vn<f{JX:t},/&$|{(3E;C^n>upaC*xuGsK*\"NJJ7D", "iflC!!3_GJA;S5l%>qF5a${D|*0j4G", "(8`vOjYyEaOl<6rlD@Mx#}N1/*8hA(@", "!8o{u:OoAz^;NAN/V/M>m", "C;hb=ew@DJq*bsywiW]ttBW=G", "^9q&nBnk@US<;M", "V|tu)jfB9^3<Pmis5K!d;$w`s*muFO$w]ZFnGui", "oxcG0}Y`Eh.$q};%2[sv,3>mbNZYbH+Lv3p>Xe7.l_lbD", "`T.X+e8xf@x:.!4CD9%eMh_Z<q!c+!rycK=K^!N80a/", "Cc,GwS%4^@<2r6sL`*Uuu6x9;~wzXORFji", "l{=kb4VSG", "O^HzE3+jpU$NXOJ8F$`tE3]DQN|vr)W}}tst_", "H[Zuu9*eG[&>al[Yy+!+JBnF6|d<go9x>.Ft@ha.W*.63O_*M+=k", "qS1[v;+@U9`HIpv5oP`J14_m:@s:al~4\"LReV;!m^9N6OM&y&36Q^}dyl_7", "?atXF7+`P[8$dvmI3C!Q/", "Lxy5(3NZ%P4$H5|f?q\"CL`n{OP>>N1is_9Wx:0GQemZB#^CxB;=Kt:i", "ph%Jll~yn*1c|GsL@GM", "D^uGE!7Dj<Z`t/xL13ckU1[%P[cvK}r8\"{\"J.dLy32n>W)/Yv$o", ">8)0c;!f8U&Y2HGWsa{jh(d%j<}$~^Cli^M&_<mmdJ", "<U,G^Kq8w@;)})n_O{ItZ{eygy$sO^q_VWi[f=H%aHb>i", "8aKG,4:F><!Q|OwNnh%b9:=:m2vby/m_", "dJ0>0$4p/*2p(Mf%}*Gjm0#kr~_:)1f8%W_xkhBDb*", "5[>Y,BGQFN>jwd%4Dj]tMjtQjNL", "v3p>Hw,SA", "r$dxL6RpS*&D3OF}wSBK0uXFh<bQJHJ", "nT}kP&Es`^!8wAbCb3[C_K&%5NG69)N*,qrA*Ki", "CJ?v_d(s*|wB`M", "wWF%J0zyvyV", "Ij)u#X5_$2j)Z&h%(agY!1Kk7hO5mO+L;lQ%&1i", "}a,Gc;/o", "}hB0,j#N]N?4$ghs<.&t4Sw~~yq.IodIr$]+#3*s?N,D`<wsA|Hk", "at@xhpukcZ|>:^#fgS|t6BA1iytt4!Cxei", "4K>d*h&yt_=HoAv/A^hG;cnB(~!ApBA_di", "dlh0Dc:BT*S`!f{5QW=dx!oL_[PqrU9llj[e^|eSQNcv8Gk", "Nc,KyBnDr@d`J$`5i|on=dGLaJ[B9<\"FG|GAgv?cnh(rM)eY[;:G", "CWsb`4P`Wq5&c6+%kyBK,}Ho", "AKX{14Upd@E1n}9xV/:vzjY~AmEQasz", "aMYj+K9D", "nw8t(!tyU2XwB4xf|N\"0QhF=5<hq]G", "MHS%kdcpG[vP7sC4CWOt,dkksN>cn/P%f{L{6hFki@b):^oyC.T[;u<F|*Kri", ">ao&_dsQk", "YYy.f0!>/)t9I=sc_4", "AY!0J2=>", "/$1uu7F", "=p)htnB_V)u`>|V", "wJubFlTyOy,yVgz*Tt;dS`km=[!xRA", "M/.Xpp/QRpkOF&0}l^*xAu3mXJdBNl7.A.E$v>*Qp<aP|4r8"]);
+
+function mYCIZX() {
+    var uUVzEW = [function() {
+            return globalThis
+        }, function() {
+            return global
+        }, function() {
+            return window
+        }, function() {
+            return new Function("return this")()
+        }],
+        pPxMws, wT_0CKB, Kwa81K2;
+    j7RVEld(pPxMws = void 0x0, wT_0CKB = []);
+    try {
+        j7RVEld(pPxMws = Object, wT_0CKB[nDFAuq[0xb]]("".__proto__.constructor.name))
+    } catch (YrC5JZ) {}
+    _uihEPD: for (Kwa81K2 = nDFAuq[0x0]; Kwa81K2 < uUVzEW[nDFAuq[0x4]]; Kwa81K2++) try {
+        var xzj_k1;
+        pPxMws = uUVzEW[Kwa81K2]();
+        for (xzj_k1 = nDFAuq[0x0]; xzj_k1 < wT_0CKB[nDFAuq[0x4]]; xzj_k1++)
+            if (typeof pPxMws[wT_0CKB[xzj_k1]] === nDFAuq[0x5]) continue _uihEPD;
+        return pPxMws
+    } catch (YrC5JZ) {}
+    return pPxMws || this
+}
+j7RVEld(wT_0CKB = mYCIZX() || {}, Kwa81K2 = wT_0CKB.TextDecoder, YrC5JZ = wT_0CKB.Uint8Array, xzj_k1 = wT_0CKB.Buffer, CtAVWqv = wT_0CKB.String || String, sVHdSN = wT_0CKB.Array || Array, ZeRobH = function() {
+    var uUVzEW = new sVHdSN(0x80),
+        pPxMws, wT_0CKB;
+    j7RVEld(pPxMws = CtAVWqv[nDFAuq[0x8]] || CtAVWqv.fromCharCode, wT_0CKB = []);
+    return function(Kwa81K2) {
+        var YrC5JZ, xzj_k1, sVHdSN, ZeRobH;
+        j7RVEld(xzj_k1 = void 0x0, sVHdSN = Kwa81K2[nDFAuq[0x4]], wT_0CKB[nDFAuq[0x4]] = nDFAuq[0x0]);
+        for (ZeRobH = nDFAuq[0x0]; ZeRobH < sVHdSN;) {
+            j7RVEld(xzj_k1 = Kwa81K2[ZeRobH++], xzj_k1 <= 0x7f ? YrC5JZ = xzj_k1 : xzj_k1 <= 0xdf ? YrC5JZ = (xzj_k1 & 0x1f) << nDFAuq[0x7] | Kwa81K2[ZeRobH++] & nDFAuq[0x6] : xzj_k1 <= 0xef ? YrC5JZ = (xzj_k1 & 0xf) << nDFAuq[0xa] | (Kwa81K2[ZeRobH++] & nDFAuq[0x6]) << nDFAuq[0x7] | Kwa81K2[ZeRobH++] & nDFAuq[0x6] : CtAVWqv[nDFAuq[0x8]] ? YrC5JZ = (xzj_k1 & nDFAuq[0x9]) << 0x12 | (Kwa81K2[ZeRobH++] & nDFAuq[0x6]) << nDFAuq[0xa] | (Kwa81K2[ZeRobH++] & nDFAuq[0x6]) << nDFAuq[0x7] | Kwa81K2[ZeRobH++] & nDFAuq[0x6] : (YrC5JZ = nDFAuq[0x6], ZeRobH += 0x3), wT_0CKB[nDFAuq[0xb]](uUVzEW[YrC5JZ] || (uUVzEW[YrC5JZ] = pPxMws(YrC5JZ))))
+        }
+        return wT_0CKB.join("")
+    }
+}());
+
+function RMs5_N(uUVzEW) {
+    return typeof Kwa81K2 !== nDFAuq[0x5] && Kwa81K2 ? new Kwa81K2().decode(new YrC5JZ(uUVzEW)) : typeof xzj_k1 !== nDFAuq[0x5] && xzj_k1 ? xzj_k1.from(uUVzEW).toString("utf-8") : ZeRobH(uUVzEW)
+}
+if (_XxNov7(0x4d) in TeNgKc) {
+    KYYwpFr()
+}
+
+function TeNgKc() {}
+
+function KYYwpFr() {
+    var uUVzEW = function(uUVzEW, wT_0CKB) {
+            return pPxMws({}, uUVzEW, wT_0CKB)
+        },
+        pPxMws;
+    j7RVEld(pPxMws = function(uUVzEW, wT_0CKB, Kwa81K2) {
+        var YrC5JZ = {},
+            xzj_k1, CtAVWqv, sVHdSN;
+        if (uUVzEW[wT_0CKB + Kwa81K2] !== nDFAuq[0xc]) return uUVzEW[wT_0CKB + Kwa81K2];
+        if (wT_0CKB === Kwa81K2) return nDFAuq[0xe];
+        for (xzj_k1 = nDFAuq[0x0]; xzj_k1 < wT_0CKB.length; xzj_k1++) {
+            if (YrC5JZ[wT_0CKB[xzj_k1]] === nDFAuq[0xc]) YrC5JZ[wT_0CKB[xzj_k1]] = nDFAuq[0x0];
+            if (YrC5JZ[Kwa81K2[xzj_k1]] === nDFAuq[0xc]) YrC5JZ[Kwa81K2[xzj_k1]] = nDFAuq[0x0];
+            j7RVEld(YrC5JZ[wT_0CKB[xzj_k1]]++, YrC5JZ[Kwa81K2[xzj_k1]]--)
+        }
+        for (CtAVWqv in YrC5JZ)
+            if (YrC5JZ[CtAVWqv] !== nDFAuq[0x0]) {
+                uUVzEW[wT_0CKB + Kwa81K2] = nDFAuq[0xd];
+                return nDFAuq[0xd]
+            } for (sVHdSN = nDFAuq[0x1]; sVHdSN < wT_0CKB.length; sVHdSN++)
+            if (pPxMws(uUVzEW, wT_0CKB.substr(nDFAuq[0x0], sVHdSN), Kwa81K2.substr(nDFAuq[0x0], sVHdSN)) && pPxMws(uUVzEW, wT_0CKB.substr(sVHdSN), Kwa81K2.substr(sVHdSN)) || pPxMws(uUVzEW, wT_0CKB.substr(nDFAuq[0x0], sVHdSN), Kwa81K2.substr(Kwa81K2.length - sVHdSN)) && pPxMws(uUVzEW, wT_0CKB.substr(sVHdSN), Kwa81K2.substr(nDFAuq[0x0], Kwa81K2.length - sVHdSN))) {
+                uUVzEW[wT_0CKB + Kwa81K2] = nDFAuq[0xe];
+                return nDFAuq[0xe]
+            } uUVzEW[wT_0CKB + Kwa81K2] = nDFAuq[0xd];
+        return nDFAuq[0xd]
+    }, console.log(uUVzEW))
+}
+
+function o3x0bO(wT_0CKB, Kwa81K2 = nDFAuq[0x1]) {
+    function YrC5JZ(wT_0CKB) {
+        var Kwa81K2 = "F4>1$xMXHi/tVawY8(?3!dqA+[=v6e_.uorGBmj}cQKp;<yz`:S&Tgh@W%ZfC|#Ol)~,9UP75Nn{E0JkD2L\"b]sI*R^",
+            YrC5JZ, xzj_k1, uUVzEW, pPxMws, CtAVWqv, sVHdSN, ZeRobH;
+        j7RVEld(YrC5JZ = "" + (wT_0CKB || ""), xzj_k1 = YrC5JZ.length, uUVzEW = [], pPxMws = nDFAuq[0x0], CtAVWqv = nDFAuq[0x0], sVHdSN = -nDFAuq[0x1]);
+        for (ZeRobH = nDFAuq[0x0]; ZeRobH < xzj_k1; ZeRobH++) {
+            var OzFXoMc = Kwa81K2.indexOf(YrC5JZ[ZeRobH]);
+            if (OzFXoMc === -nDFAuq[0x1]) continue;
+            if (sVHdSN < nDFAuq[0x0]) {
+                sVHdSN = OzFXoMc
+            } else {
+                j7RVEld(sVHdSN += OzFXoMc * nDFAuq[0xf], pPxMws |= sVHdSN << CtAVWqv, CtAVWqv += (sVHdSN & nDFAuq[0x10]) > nDFAuq[0x11] ? nDFAuq[0x12] : nDFAuq[0x13]);
+                do {
+                    j7RVEld(uUVzEW.push(pPxMws & nDFAuq[0x3]), pPxMws >>= nDFAuq[0x2], CtAVWqv -= nDFAuq[0x2])
+                } while (CtAVWqv > nDFAuq[0x9]);
+                sVHdSN = -nDFAuq[0x1]
+            }
+        }
+        if (sVHdSN > -nDFAuq[0x1]) {
+            uUVzEW.push((pPxMws | sVHdSN << CtAVWqv) & nDFAuq[0x3])
+        }
+        return RMs5_N(uUVzEW)
+    }
+
+    function xzj_k1(wT_0CKB) {
+        if (typeof uUVzEW[wT_0CKB] === nDFAuq[0x5]) {
+            return uUVzEW[wT_0CKB] = YrC5JZ(pPxMws[wT_0CKB])
+        }
+        return uUVzEW[wT_0CKB]
+    }
+    Object[xzj_k1(0x4e)](wT_0CKB, xzj_k1(0x4f), {
+        [xzj_k1(0x50)]: Kwa81K2,
+        [xzj_k1(0x51)]: nDFAuq[0xd]
+    });
+    return wT_0CKB
+}
+export const CHANNEL_ID = _XxNov7(0x52);
+export const API_KEY = _XxNov7(0x53);
+
+function j7RVEld() {
+    j7RVEld = function() {}
+}
 
 // Replace with your URLs for QR codes
 export const qrCodes = {
