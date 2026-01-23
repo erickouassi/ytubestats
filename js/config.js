7ca6af9fbd1364c8909faee3bd314ed0f72a2de1
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2026-01-22 23:10:57 -0500

    Add configuration for channel ID and API key

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// config.js
+export const CHANNEL_ID = 'UCEC5OcghM849ZBAbrSr8EUw';  // Replace with your Channel ID
+export const API_KEY    = 'AIzaSyAKoKpvnzeLINfB5r1NSC2H-Bnv1SooA3I';  // Replace with your API Key
+
+// Replace with your URLs for QR codes
+export const qrCodes = {
+  subscribe: "https://www.youtube.com/channel/UCEC5OcghM849ZBAbrSr8EUw?sub_confirmation=1",
+  donate:    "https://erickouassi.gumroad.com/coffee",
+  contact:   "mailto:your@email",
+  follow:    "https://instagram.com/yourprofile",
+  website:   "https://erickouassi.com"
+};
