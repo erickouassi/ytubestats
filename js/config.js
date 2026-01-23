1abe9aa53885a50feb48b0dd03f751a42d5e874d
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2026-01-23 09:33:30 -0500

    Refactor config.js to export constants and URLs

--- js/config.js
+++ js/config.js
@@ -16,124 +16,15 @@ Use the commented code above to replace the code in the section below.
 Make sure the replacement is exact and preserves all functionality.  
 */
 
+// config.js
+export const CHANNEL_ID = 'UCEC5OcghM849ZBAbrSr8EUw';  // Replace with your Channel ID
+export const API_KEY    = 'AIzaSyAKoKpvnzeLINfB5r1NSC2H-Bnv1SooA3I';  // Replace with your API Key
 
-var GCmv1x, Soixz1L, oKaNwn, R1r6iv6, y6z8oB, MYSS9x2, Qi9yvA, SkieUBk, tdkh9sm;
-const Ve2GYJo = [0x0, 0x1, 0x8, 0xff, "length", "undefined", 0x3f, 0x6, "fromCodePoint", 0x7, 0xc, "push", 0x5b, 0x1fff, 0x58, 0xd, 0xe];
-
-function _n89U7(GCmv1x) {
-    var Soixz1L = "z|}Egb+yQYdT_*nfa4?o)mtX,pS03#v6F1Bj;H^Wlq{srD$[C`kw2Z~Vx5AN97.:]/c=8@\"OLIGUP<>e!uK%&JM(Rhi",
-        oKaNwn, R1r6iv6, y6z8oB, MYSS9x2, Qi9yvA, SkieUBk, tdkh9sm;
-    Jm2hmR(oKaNwn = "" + (GCmv1x || ""), R1r6iv6 = oKaNwn.length, y6z8oB = [], MYSS9x2 = Ve2GYJo[0x0], Qi9yvA = Ve2GYJo[0x0], SkieUBk = -Ve2GYJo[0x1]);
-    for (tdkh9sm = Ve2GYJo[0x0]; tdkh9sm < R1r6iv6; tdkh9sm++) {
-        var _n89U7 = Soixz1L.indexOf(oKaNwn[tdkh9sm]);
-        if (_n89U7 === -Ve2GYJo[0x1]) continue;
-        if (SkieUBk < Ve2GYJo[0x0]) {
-            SkieUBk = _n89U7
-        } else {
-            Jm2hmR(SkieUBk += _n89U7 * Ve2GYJo[0xc], MYSS9x2 |= SkieUBk << Qi9yvA, Qi9yvA += (SkieUBk & Ve2GYJo[0xd]) > Ve2GYJo[0xe] ? Ve2GYJo[0xf] : Ve2GYJo[0x10]);
-            do {
-                Jm2hmR(y6z8oB.push(MYSS9x2 & Ve2GYJo[0x3]), MYSS9x2 >>= Ve2GYJo[0x2], Qi9yvA -= Ve2GYJo[0x2])
-            } while (Qi9yvA > Ve2GYJo[0x9]);
-            SkieUBk = -Ve2GYJo[0x1]
-        }
-    }
-    if (SkieUBk > -Ve2GYJo[0x1]) {
-        y6z8oB.push((MYSS9x2 | SkieUBk << Qi9yvA) & Ve2GYJo[0x3])
-    }
-    return Buvvtp(y6z8oB)
-}
-
-function EoSUFa(oKaNwn) {
-    if (typeof GCmv1x[oKaNwn] === Ve2GYJo[0x5]) {
-        return GCmv1x[oKaNwn] = _n89U7(Soixz1L[oKaNwn])
-    }
-    return GCmv1x[oKaNwn]
-}
-Jm2hmR(GCmv1x = {}, Soixz1L = ["|FFvF!2Gl)@\")99{,O%Q", "5O>I:Bf*%Yw.;0l^o|", ".Yr<Y5.,(]*dgfOoEY|+x:+,n?2!o0};Hw~2]L\"A;5TBFO1XjHW22wN|", "M\"@6JD#Ze_K=9%>S{476hJB?2oS)Vn|s;X~3<;e#a9{", "V8,Yx9Ml4YM\"5HrSzYM2eP,!1:4/@L!By5:~t", "4lo<i5^}", "9/W~cZGl|Y^B:n@pF5lw0NbuPnM\"yO)37._6[<V|fcmPPC0XX|", "#sT<FOD[|?Vc*CZq4F[I?%~8C_@g;[i3U8f+LB!+[m8IVNx{c~2d", "^s8bDx_MtAK=7gDo", "(NT<nk2l+A/<B*OHl/@kplv?C5O3Dgq^G+6Y", ":N1+!.L+!_V\"1HA;1Gqv4Vz", "Q:N#|V[|", "6B8#v2=5kfmeWAWoiz", "^~rdswz", "dO\"VswX[vmd+0n14~/VgvOSvU4avfk]??5$y", "#8pL$:.?r4", "*HxYDwz", "iQB01>+{c~Z!Bpb,*`8y^In,.fXPTS!BClpUPVg]<ZetqLi{>T9ZM5r2e:3*|", "&D8#{Ia,e?[,%n6;4:WYK%l;o5Tddb=H|9tIU@$?|9;BCEy", "F9mV:Bmo+o~?>`;vx+D64;>lcf", "./!+bZu#j~6e$*(#*8$YZZ0#*c{cHa$W.E`0t@g,i)@<,Od", "_89#]Wew(AwPX0vv)S^Pc.Zy8o]:+uY", "Fs[`8L*yvc[4s^j?mS$g{~tMF_Z", "<HtVKDDJ#m`xRNp3b#X`Ie!BDxBPwN64BX6I*", "NSGwDKhq<xE*4;Yo(Wd0)^e|PT^IDaMa:~OI!^Zq7Zcc@CW^ACi#1joo+", "uBCYk$z", "v/tP@BL?[:~!Jg/{`_,+i5j:fm6di`eS3#OIS~W@t]{,#g", "1COg$A(X%Y.IeEW?iZ;y", "!n)<BJD[pn|>dui#X$8S`H@q|c$dM`B;q)P<%>Iwk5m@POW6X$<y", "jG^VJ@I5qAX4[nRaV5Z2o>8!b*;djkCBzs_#>.{vkYp*90.{_|", "18Hg)^K$)5L!9EXB1_,YVHI|", "qSV`c9dQ)]]jTKxB1_9ZsU^AKfHPj%.4y~vYY9X#b", "Eg\"3vl]MA.3@Ap~l25Y~>5?,{)^e0kzvOHE", "~DQw_G?2?9C3oS{^0~}0nki29NBL|", "t+@k%%q5i.d~j\"\"?", "=8&S+LIq:Q9<|", "tlSw%Dc$q4&<EKW6s4M2d^R8;~fX)OS{y`(YROB!!9]\"5*=?e/^ye.k?a?_)|", "%H=Qlj|p=o;30n34@XTLdC[|94m@mu,B[5iZs~z", "n5Zdm2i82]>|sS;?rH!YU@7X1f^@[^A3@CcZ\"kt$y*Eeef^p", "xO(~@C~!O*/2c@^p?_+6(1z", "GCtI<uo[L)H6.p{6Q_|+D!p@Z*nA(}165w26pOTU$xUZb&_W0fR`o5kl&o", "b~~I.e[*5/T7)fXqxCbPo@7[SN4CF[^,`_F~cZ4|", "1+$PvILvc5EX2Nk3.CcQJDpjY4p,3EQHt+VgVA(#hnFPpuUW\"4Mgl[U|", "gcnPFN7[H~;m%S|4=I{Uuu<w+", "UNvYM1J4v:OI6nd", "RC,wA$5|L/?vrNlSN~KFC\".$P4+8`a5#!BlY", ";fzYjI*|%_nv!E{W5X^24@Tpbdi;]N/,qHQYi^7*B_Fez", "GCm6f^K$J*KQqgE4H4OYa5W[Jod8}uR{Em\"3)%;;&YR$~`#j=Iz1?", "pSLYHloZ$mnv<,|viWHGvO=#h)h]Kp0qN.PkkA%|sQ~`:n74q..VQG7[+", "=873)lo#Tn#2d7c,|?gd1~)!s/.`jSF,M/<3B7x!rxL\",E", "epIww:G,5.u\"U\"h#.n#ye;*yi/5y/gLSgs\"g[<=#pnJ9;E", "+8)ydPuym~6L]9Po#):3Q.2MefZIDpNXt~|", "P.^3HjbphA\"||", "s5ibW8Xuz9Qwma=H~XFvLP:[S4,)=nZ3RwuQ", "+S[299gva_DPF%=pBB+P];$2NQzYHC!BQF[IqI4*45V!/@g6", "}#]Yq!z", "gYUZp5gM>QTVfkml3Xegp", "tF(3jjI*U/5m_0QH%/P#Blz", "AN%Q]e.v$xQa9%CS4Oh+Djv!o5WVJ,Xqnf=b4", "(W!YJ%K2)o5x`CAqp$96%.pqh/|[pCc4</#y:e%*Dny}Guu0`)>2VwRB+", "I6wLSN5*PQwPHgIW4:$PuDW5]:)PIu},;STLa", "E5EFY.zv:?,+@CBvSB\"OnGq|=9YA|", "7~{<,>uwin`x_Ws^^)R~]Ck]GnB@{7+?+s8dj~A8lA6e)EYW?5Zg^`^}", "T:%<+.Xo6c3P0NjHV~y22U$_?f5", "L)@k#J$_v?a))N,BV8F`6>B?Z~Bc5gRjj8O`[\"FG[mTCLOK3mGj#1J7|", "dF~2hIM,B~VPn^:X`w6P^HwoE]1BvbW^S/f1@er_v?oE@LJ31Y}", "dl,Y^K|:15w@A*hXj_4~@G?$=9EKNAUo+gZ6J1tGsN4@TE", "U/|>0jfyEo1LUn23WYW3}&r2i4", "U+kFAB._~*}~sn(jR.F`=emyu?\"jpa!W</:~!.RB6cq!_O&lD+?w,", "qwjLxWi!]9`.{*34a5QY1N3;$Q%knHHot9&O|;{ljox", "G+j<kK*|E", "|YE<^~}A9QR\"YC1,*$=b!2t$9xTVe}R{]B~V$<u5k_3*`gOp=.$2K5,$L/B", "O/pv%Nw#x/veR,E6rXLYAwkvC5l3l[6HNX,~.AS2r47?Q%pjiEgb$`z", "\".=#j~Ow4:I|/,.X+FNZ=$LMF_z>z0SjiCP#[:b|kfiQ>pIWa|", "I)y2Kk!$]?E7F[1;_:FYwK|(+AoU=KE45E]ILCz", "`DV+NAtQ<N8:[nMlqHVg|93;o_F/)07X#8.6iN98`fv*uCn", "].bVB2qy+dh\"9923D.~Y5wLvHAB", "y#UZ@GHjF:>tIC^,zjb6hN}]lAl", "gcnVR[D[DZr@1*!W!TwQ3>r$VA=Tk*W^Z+s#d9z", ",OzYi%c$U)vdNntjv~5~`:u#*NFL4HY", "1wn2iN~2kY$@z", "(WN<HJ*q^*km`a3qSBq0EGn$j9*V9E", "4OqU_kSvJAPT?Hg^+X}", "X~}#3[VySnWV[}", "??Gr|\"u<DL5dM8`4;(", "P?u\"F08<", "DE7CC@{", "8^Lb5Xv;VLCJ<qV", "BYVIqHx?a?>?r[d)x+8kl!y*J~%#tb", "ESX1AASQtAyaqLP^Hf)#bVD*1Yk@4H7XbXiC`<)QAN/c5;=p", "H5md4u7J0Z|>@LV{09=Ft@i!l.K:Pj];qloFmPf#<x~gdu%BAXI+c;[57xaCO^?l.n|U5r9v#.8`HL|;LwTF<u]M^]X@z", "H5md4u7J0Z!\"j^@H09yP4uyu*:J:L%XBrlmP(P%4Zo|~/9_", "|g,Y!u]A#cnU}bVj|g,Y0", "H5md4u7J0Zp/yk,B\"w+6NF2Mt]W!n7tlewI~TG$v+", "H5md4u7J0Z!\"j^@H09yP4uyul.K:|"]);
-
-function WJyw_C() {
-    var GCmv1x = [function() {
-            return globalThis
-        }, function() {
-            return global
-        }, function() {
-            return window
-        }, function() {
-            return new Function("return this")()
-        }],
-        Soixz1L, oKaNwn, R1r6iv6;
-    Jm2hmR(Soixz1L = void 0x0, oKaNwn = []);
-    try {
-        Jm2hmR(Soixz1L = Object, oKaNwn[Ve2GYJo[0xb]]("".__proto__.constructor.name))
-    } catch (y6z8oB) {}
-    YkfU0DT: for (R1r6iv6 = Ve2GYJo[0x0]; R1r6iv6 < GCmv1x[Ve2GYJo[0x4]]; R1r6iv6++) try {
-        var MYSS9x2;
-        Soixz1L = GCmv1x[R1r6iv6]();
-        for (MYSS9x2 = Ve2GYJo[0x0]; MYSS9x2 < oKaNwn[Ve2GYJo[0x4]]; MYSS9x2++)
-            if (typeof Soixz1L[oKaNwn[MYSS9x2]] === Ve2GYJo[0x5]) continue YkfU0DT;
-        return Soixz1L
-    } catch (y6z8oB) {}
-    return Soixz1L || this
-}
-Jm2hmR(oKaNwn = WJyw_C() || {}, R1r6iv6 = oKaNwn.TextDecoder, y6z8oB = oKaNwn.Uint8Array, MYSS9x2 = oKaNwn.Buffer, Qi9yvA = oKaNwn.String || String, SkieUBk = oKaNwn.Array || Array, tdkh9sm = function() {
-    var GCmv1x = new SkieUBk(0x80),
-        Soixz1L, oKaNwn;
-    Jm2hmR(Soixz1L = Qi9yvA[Ve2GYJo[0x8]] || Qi9yvA.fromCharCode, oKaNwn = []);
-    return function(R1r6iv6) {
-        var y6z8oB, MYSS9x2, SkieUBk, tdkh9sm;
-        Jm2hmR(MYSS9x2 = void 0x0, SkieUBk = R1r6iv6[Ve2GYJo[0x4]], oKaNwn[Ve2GYJo[0x4]] = Ve2GYJo[0x0]);
-        for (tdkh9sm = Ve2GYJo[0x0]; tdkh9sm < SkieUBk;) {
-            Jm2hmR(MYSS9x2 = R1r6iv6[tdkh9sm++], MYSS9x2 <= 0x7f ? y6z8oB = MYSS9x2 : MYSS9x2 <= 0xdf ? y6z8oB = (MYSS9x2 & 0x1f) << Ve2GYJo[0x7] | R1r6iv6[tdkh9sm++] & Ve2GYJo[0x6] : MYSS9x2 <= 0xef ? y6z8oB = (MYSS9x2 & 0xf) << Ve2GYJo[0xa] | (R1r6iv6[tdkh9sm++] & Ve2GYJo[0x6]) << Ve2GYJo[0x7] | R1r6iv6[tdkh9sm++] & Ve2GYJo[0x6] : Qi9yvA[Ve2GYJo[0x8]] ? y6z8oB = (MYSS9x2 & Ve2GYJo[0x9]) << 0x12 | (R1r6iv6[tdkh9sm++] & Ve2GYJo[0x6]) << Ve2GYJo[0xa] | (R1r6iv6[tdkh9sm++] & Ve2GYJo[0x6]) << Ve2GYJo[0x7] | R1r6iv6[tdkh9sm++] & Ve2GYJo[0x6] : (y6z8oB = Ve2GYJo[0x6], tdkh9sm += 0x3), oKaNwn[Ve2GYJo[0xb]](GCmv1x[y6z8oB] || (GCmv1x[y6z8oB] = Soixz1L(y6z8oB))))
-        }
-        return oKaNwn.join("")
-    }
-}());
-
-function Buvvtp(GCmv1x) {
-    return typeof R1r6iv6 !== Ve2GYJo[0x5] && R1r6iv6 ? new R1r6iv6().decode(new y6z8oB(GCmv1x)) : typeof MYSS9x2 !== Ve2GYJo[0x5] && MYSS9x2 ? MYSS9x2.from(GCmv1x).toString("utf-8") : tdkh9sm(GCmv1x)
-}
-
-function PV9Y0n(oKaNwn, R1r6iv6 = Ve2GYJo[0x1]) {
-    function y6z8oB(oKaNwn) {
-        var R1r6iv6 = "{(<7Ew)s!9D5V#U?p,x%u/RPgQ8>ha;rCt_jv6*24z~^fmGOJKl}o[b&Y1Z|.qIBWLecd=N@y]X+T\"F$n0AH3k`Mi:S",
-            y6z8oB, MYSS9x2, GCmv1x, Soixz1L, Qi9yvA, SkieUBk, tdkh9sm;
-        Jm2hmR(y6z8oB = "" + (oKaNwn || ""), MYSS9x2 = y6z8oB.length, GCmv1x = [], Soixz1L = Ve2GYJo[0x0], Qi9yvA = Ve2GYJo[0x0], SkieUBk = -Ve2GYJo[0x1]);
-        for (tdkh9sm = Ve2GYJo[0x0]; tdkh9sm < MYSS9x2; tdkh9sm++) {
-            var _n89U7 = R1r6iv6.indexOf(y6z8oB[tdkh9sm]);
-            if (_n89U7 === -Ve2GYJo[0x1]) continue;
-            if (SkieUBk < Ve2GYJo[0x0]) {
-                SkieUBk = _n89U7
-            } else {
-                Jm2hmR(SkieUBk += _n89U7 * Ve2GYJo[0xc], Soixz1L |= SkieUBk << Qi9yvA, Qi9yvA += (SkieUBk & Ve2GYJo[0xd]) > Ve2GYJo[0xe] ? Ve2GYJo[0xf] : Ve2GYJo[0x10]);
-                do {
-                    Jm2hmR(GCmv1x.push(Soixz1L & Ve2GYJo[0x3]), Soixz1L >>= Ve2GYJo[0x2], Qi9yvA -= Ve2GYJo[0x2])
-                } while (Qi9yvA > Ve2GYJo[0x9]);
-                SkieUBk = -Ve2GYJo[0x1]
-            }
-        }
-        if (SkieUBk > -Ve2GYJo[0x1]) {
-            GCmv1x.push((Soixz1L | SkieUBk << Qi9yvA) & Ve2GYJo[0x3])
-        }
-        return Buvvtp(GCmv1x)
-    }
-
-    function MYSS9x2(oKaNwn) {
-        if (typeof GCmv1x[oKaNwn] === Ve2GYJo[0x5]) {
-            return GCmv1x[oKaNwn] = y6z8oB(Soixz1L[oKaNwn])
-        }
-        return GCmv1x[oKaNwn]
-    }
-    Object[MYSS9x2(0x54)](oKaNwn, MYSS9x2(0x55), {
-        [MYSS9x2(0x56)]: R1r6iv6,
-        [MYSS9x2(0x57)]: !0x1
-    });
-    return oKaNwn
-}
-export const CHANNEL_ID = EoSUFa(Ve2GYJo[0xe]);
-export const API_KEY = EoSUFa(0x59);
-Jm2hmR(EoSUFa(0x5a), EoSUFa(Ve2GYJo[0xc]), EoSUFa(0x5c), EoSUFa(0x5d));
-
-function Jm2hmR() {
-    Jm2hmR = function() {}
-}
-EoSUFa(0x5e);
+// Replace with your URLs for QR codes
+export const qrCodes = {
+  subscribe: "https://www.youtube.com/channel/UCEC5OcghM849ZBAbrSr8EUw?sub_confirmation=1",
+  donate:    "https://erickouassi.gumroad.com/coffee",
+  contact:   "mailto:your@email",
+  follow:    "https://instagram.com/yourprofile",
+  website:   "https://erickouassi.com"
+};
