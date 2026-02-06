5ab93e32a75c07fd5533d451a2f64fcb58233eed
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2026-02-05 22:50:00 -0500

    Add files via upload

--- config/config.js
+++ config/config.js
@@ -1,8 +1,15 @@
 const userLicenseKey = "XP-123-ABC";   // Replace with your userLicenseKey
 
+// Make it global so badge.js and license.js can see it 
+window.userLicenseKey = userLicenseKey;
+
 /*  
 You'll need a valid userLicenseKey before using this project.  
-Grab one at licensekey.com  
+Grab one at https://erickouassi.gumroad.com/l/ytubestats  
 */
 
 
+
+// console.log("[CONFIG] Loaded userLicenseKey =", userLicenseKey);
+// console.log("[CONFIG] Window has userLicenseKey =", window.userLicenseKey);
+// console.log("[CONFIG] Document readyState =", document.readyState);
